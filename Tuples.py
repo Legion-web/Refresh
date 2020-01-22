770b7cb87823e95fbb9165369a9933f4512e706c
Legion-web <flashabcd1@gmailcom>
2020-01-22 06:13:50 +0530

    Added new sets of files

--- Tuples.py
+++ Tuples.py
@@ -1,3 +1,5 @@
 #Tuples
+#Tuples are objects which cannot be edited once created
+#However they can be changed is they are enclosed in a list i.e. enclosed between two square brackets []
 coordinates=(5,8,7)
 print(coordinates[1])
---
+++ return function.py
@@ -0,0 +1,6 @@
+def abcd():
+    name=input("Please enter your name:")
+    name=name.capitalize()
+    age=input("Please enter your age:")
+    print("Your name is "+name+" and your age is "+age+" !")
+abcd()
