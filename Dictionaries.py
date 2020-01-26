dbfdea231fe830fd9750cac70afa6254ca59c626
Legion-web <flashabcd1@gmailcom>
2020-01-26 10:18:27 +0530

    New projects

---
+++ Dictionaries.py
@@ -0,0 +1,41 @@
+monthConversions = {
+
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+key = input("Enter a position of a month:")
+if key == "1" or "Jan":
+    print(monthConversions["Jan"])
+elif key == "2" or "Feb":
+    print(monthConversions["Feb"])
+elif key == "3" or "Mar":
+    print(monthConversions["Mar"])
+elif key == "4" or "Apr":
+    print(monthConversions["Apr"])
+elif key == "5" or "May":
+    print(monthConversions["May"])
+elif key == "6" or "Jun":
+    print(monthConversions["Jun"])
+elif key == "7" or "Jul":
+    print(monthConversions["Jul"])
+elif key == "8" or "Aug":
+    print(monthConversions["Aug"])
+elif key == "9" or "Sep":
+    print(monthConversions["Sep"])
+elif key == "10" or "Oct":
+    print(monthConversions["Oct"])
+elif key == "11" or "Nov":
+    print(monthConversions["Nov"])
+elif key == "12" or "Dec":
+    print(monthConversions["Dec"])
