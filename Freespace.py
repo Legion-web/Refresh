bb43089c756a271f0ff45da0c467e6fd8584a499
Legion-web <flashabcd1@gmailcom>
2020-02-13 01:39:46 +0530

    Files !

--- Freespace.py
+++ Freespace.py
@@ -15,10 +15,9 @@ def run_test(questions):
     score = 0
     for question in questions:
         answer = input(question.prompt)
-        if answer == questions.answers:
+        if answer == questions.answer:
             score += 1
 
     print("You got " + str(score) + "/" + str(len(questions)) + "correct")
-
 
 run_test(questions)
---
+++ Ub.py
@@ -0,0 +1,33 @@
+def play():
+    ply=input("Would you like to run the programme(y/n) ?:")
+    if ply=="y" or ply=="Y":
+        util()
+    else:
+        exit()
+def util():
+    import random
+    inp=""
+    ext=False
+    while not ext:
+        inp=input("Enter a string :")
+        ran_number=random.randint(1,10)
+        abcd= str(inp.splitlines()) * ran_number
+        print("The number generated was "+str(ran_number)+" and your result is :"+str(abcd))
+        agn()
+    else:
+        ext = True
+    if ext:
+        exit()
+    else:
+        print("An error occured")
+
+def agn():
+    again=input("Would you like to play again ?: ")
+    if again =="y" or again =="Y":
+        util()
+    else:
+        exit()
+
+util()
+agn()
+play()
